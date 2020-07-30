572e42cd723c3a36ddcd3170a491d4237dceeca0
andresoaea <constantin.andresoaea@gmail.com>
2020-07-30 13:01:33 +0300

    Updated User table miration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
@@ -14,10 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->increments('id');
+            $table->string('username');
+            $table->string('firstname');
+            $table->string('lastname')->nullable();
+            $table->string('picture')->nullable();
+            $table->bigInteger('fbid')->nullable();
+            $table->string('role')->default('user');
+            $table->timestamp('last_login')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
