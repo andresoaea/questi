dc56374b978fc596d83bdfa16b28b39d4d194aff
andresoaea <constantin.andresoaea@gmail.com>
2020-07-30 13:10:14 +0300

    Created required models + migrations

---
+++ database/migrations/2020_07_30_100532_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('title');
+            $table->string('slug');
+            $table->longText('description')->nullable();
+            $table->integer('category');
+            $table->integer('author');
+            $table->integer('best_answer')->nullable();
+            $table->json('reactions')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
